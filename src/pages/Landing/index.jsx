71e75606abc790a3cdaea3743ae6271bffd7666d
SimplySylvia <daltonhart.j@gmail.com>
2023-05-24 09:53:52 -0700

    :tada: (project) sets up local development workflow with vite

---
+++ src/pages/Landing/index.jsx
@@ -0,0 +1,10 @@
+import { Header } from "@ui";
+function Landing() {
+  return <div className="Landing">
+    <Header
+      text="Modmopet"
+      subtext="Finally a modmanager for emulation."
+    />
+  </div>
+}
+export default Landing;
